caa304f77ec858893eef45990f7caefdda021115
Alexander Urizar <urizaraj@gmail.com>
2018-03-01 18:09:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
+    @post = Post.find(params[:id])
     erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
   end
 
 end
